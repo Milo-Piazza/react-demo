bb0815be0bec883ddcaca8019deb8b00faf307ea
Milo Piazza <milopiazza@MacBook-Pro-3.local>
2020-07-02 20:44:21 -0700

    Add counter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from './serviceWorker';
+import Counter from './components/counter'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter/>
   </React.StrictMode>,
   document.getElementById('root')
 );
